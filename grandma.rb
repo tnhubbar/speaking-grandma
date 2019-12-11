f2b386ea5699b6642331332c379ae81932b0ffcf
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-11 00:27:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if phrase ==  'I LOVE YOU GRANDMA!'
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase 
-    return "I LOVE YOU TOO PUMPKIN!"
+    return "NO, NOT SINCE 1938!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
 end 
